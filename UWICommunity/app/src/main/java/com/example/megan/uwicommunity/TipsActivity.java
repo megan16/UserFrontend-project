3edf407123f84ffd42a750bd96be0976c25e6db6
megan16 <megan_williams93@hotmail.com>
2016-02-29 12:59:51 -0600

    list view for tips

--- UWICommunity/app/src/main/java/com/example/megan/uwicommunity/TipsActivity.java
+++ UWICommunity/app/src/main/java/com/example/megan/uwicommunity/TipsActivity.java
@@ -6,9 +6,15 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class TipsActivity extends AppCompatActivity {
 
@@ -16,18 +22,33 @@ public class TipsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tips);
-       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-       // setSupportActionBar(toolbar);
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+        ArrayList<String> list= new ArrayList<String>();
+
+        list.add(" • Always walk with others.");
+        list.add(" • Only walk in well lit areas.");
+        list.add(" • Avoid short-cuts through poorly lit or deserted areas.");
+        list.add(" • Park in well-lit, busy areas, preferable close to security.");
+        list.add(" • Have your keys ready before approaching the car.");
+        list.add(" • Look out for anyone loitering in the car park and report your observations");
+
+        ArrayAdapter listAdapter=new ArrayAdapter<String>(this,R.layout.list_rows,list);
+        ListView listView= (ListView) findViewById(R.id.tipsList);
+        listView.setAdapter(listAdapter);
 
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-      //  getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
+    public void onBackPressed() {
+
+        Log.d("ME", "onBackPressed Called");
+        this.finish();
+
     }
 
     @Override
@@ -35,10 +56,15 @@ public class TipsActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
         Intent intent=new Intent(getApplicationContext(),SafetyActivity.class);
         startActivity(intent);
         finish();
         return true;
+
+        // return super.onOptionsItemSelected(item);
     }
+
+
 
 }
